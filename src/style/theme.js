f8e97b28891cff2de47ff2180f74e07fdda34242
EliseCho13 <dbwlspdlqj@naver.com>
2024-02-12 11:48:09 +0900

    chore(fe): initial setting

---
+++ src/style/theme.js
@@ -0,0 +1,48 @@
+import { createTheme } from "@mui/material";
+import { createGlobalStyle } from "styled-components";
+
+export const theme = {
+  color: {
+    black: "#0C0A09",
+    gachiPink: "#E11D48",
+    secondPink: "#FB7185",
+    lightGray: "#cfcfcf",
+    darkGray: "#3e3e3e",
+  },
+  device: {
+    mobile: "max-width: 620px",
+  },
+};
+
+export const muiTheme = createTheme({
+  typography: {
+    fontFamily: [
+      "Pretendard",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "system-ui",
+      "Roboto",
+      "Helvetica Neue",
+      "Segoe UI",
+      "Apple SD Gothic Neo",
+      "Noto Sans KR",
+      "Malgun Gothic",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "sans-serif",
+    ].join(","),
+  },
+});
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: ${theme.color.black};
+    color: white;
+  }
+  @media screen and (${theme.device.mobile}) {
+    html {
+      font-size: 12px;
+    }
+  }
+`;
