74e719c79474cb967d22dd69dcabd1dc58723413
cykim <cykim8811@snu.ac.kr>
2024-02-13 00:25:32 +0900

    style(navbar): add styling to the NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,36 @@
 
 import styled from "@emotion/styled";
+import React, { useState } from "react";
+import "./NavBar.css";
 
 export default function NavBar() {
   // A navbar layout
-  const Container = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 1rem;
-    background-color: var(--gray-000);
-  `;
+  const [selected, setSelected] = useState("home");
+
+  const btns = {
+    "search": {
+      icon: "fas fa-search",
+      callback: () => setSelected("search")
+    },
+    "home": {
+      icon: "fas fa-home",
+      callback: () => setSelected("home")
+    },
+    "profile": {
+      icon: "fas fa-user",
+      callback: () => setSelected("profile")
+    }
+  }
 
   return (
-    <Container>
-      <div>Home</div>
-      <div>About</div>
-      <div>Contact</div>
-    </Container>
+    <div className="navbar-container">
+      {Object.keys(btns).map((key) => (
+        <i
+          key={key}
+          className={`navicon ${selected === key ? "selected" : ""} ${btns[key].icon}`}
+          onClick={btns[key].callback}
+        />
+      ))}
+    </div>
   );
 }
