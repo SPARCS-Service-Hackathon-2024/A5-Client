2c8770d4c144ed672388c58e60ed7b715dcdbaf6
EliseCho13 <dbwlspdlqj@naver.com>
2024-02-15 01:45:22 +0900

    feat(header): add header, ok svg

---
+++ src/components/common/Header.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { MdArrowBackIosNew } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const HeaderContainer = styled.div`
+  border-bottom: 1px solid #a8a29e;
+  padding: 1.5rem;
+  font-weight: 500;
+  font-size: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #a8a29e;
+  .title {
+    color: black;
+    font-size: 1.2rem;
+  }
+`;
+
+export default function Header({ title }) {
+  const navigate = useNavigate();
+  return (
+    <HeaderContainer>
+      <MdArrowBackIosNew onClick={() => navigate(-1)} />
+      <div className="title">{title}</div>
+      <IoMdClose onClick={() => navigate("/home")} />
+    </HeaderContainer>
+  );
+}
