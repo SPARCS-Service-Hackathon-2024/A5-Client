0bec83414189e169f037adda50aa904f108214db
EliseCho13 <dbwlspdlqj@naver.com>
2024-02-14 14:56:27 +0900

    fix(listSlider): removed console log

--- src/components/search/ListSlider.js
+++ src/components/search/ListSlider.js
@@ -6,12 +6,8 @@ const ListSlider = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const aroundWalkPath = require("../../dummyData/aroundWalkPath.json");
-    console.log("path data is ", aroundWalkPath);
-
     setData(aroundWalkPath.items);
-    console.log("data is ", data); // This might still log the old value due to closure. Use useEffect for consistent logging.
   }, []);
-  console.log("data is ", data);
   return (
     <div>
       {data.map((el) => (
