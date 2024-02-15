76383204727792c595c065953b389a1ac17d4116
cykim <cykim8811@snu.ac.kr>
2024-02-15 13:50:39 +0900

    Change navbar icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,40 @@ import React, { useState } from "react";
 import "../style/NavBar.css";
 import { useNavigate, useLocation } from "react-router-dom";
 
+import searchIcon from "../assets/gray_gachi_gayu.svg";
+import homeIcon from "../assets/gray_house.svg";
+import profileIcon from "../assets/gray_person.svg";
+
+import selectedSearchIcon from "../assets/pink_gachi_gayu.svg";
+import selectedHomeIcon from "../assets/house.svg";
+import selectedProfileIcon from "../assets/person.svg";
+import { styled } from "styled-components";
+
+const Icon = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 2.5rem;
+  transform: translate(-50%, ${(props) => (props.selected ? "-50%" : "-42%")});
+  opacity: ${(props) => (props.selected ? 0 : 1)};
+  transition:
+    opacity 0.3s,
+    transform 0.3s;
+`;
+
+const SelectedIcon = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 2.5rem;
+  transform: translate(-50%, ${(props) => (props.selected ? "-50%" : "-42%")});
+  opacity: ${(props) => (props.selected ? 1 : 0)};
+  filter: drop-shadow(0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.13));
+  transition:
+    opacity 0.3s,
+    transform 0.3s;
+`;
+
 export default function NavBar() {
   const location = useLocation();
   const selected = location.pathname.split("/")[1];
@@ -13,15 +47,18 @@ export default function NavBar() {
 
   const btns = {
     search: {
-      icon: "fas fa-search",
+      icon: searchIcon,
+      selectedIcon: selectedSearchIcon,
       callback: () => clickIcon("search"),
     },
     home: {
-      icon: "fas fa-home",
+      icon: homeIcon,
+      selectedIcon: selectedHomeIcon,
       callback: () => clickIcon("home"),
     },
     profile: {
-      icon: "fas fa-user",
+      icon: profileIcon,
+      selectedIcon: selectedProfileIcon,
       callback: () => clickIcon("profile"),
     },
   };
@@ -29,11 +66,18 @@ export default function NavBar() {
   return (
     <div className="navbar-container">
       {Object.keys(btns).map((key) => (
-        <i
-          key={key}
-          className={`navicon ${selected === key ? "selected" : ""} ${btns[key].icon}`}
-          onClick={btns[key].callback}
-        />
+        <span key={key} style={{ position: "relative" }}>
+          <Icon
+            src={btns[key].icon}
+            onClick={btns[key].callback}
+            selected={selected === key}
+          />
+          <SelectedIcon
+            src={btns[key].selectedIcon}
+            onClick={btns[key].callback}
+            selected={selected === key}
+          />
+        </span>
       ))}
     </div>
   );
